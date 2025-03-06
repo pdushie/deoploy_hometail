d993c4b795d81c8f0d8ff090887958b5240d75f6
Evan Earle <evan.earle@gmail.com>
2025-03-06 18:39:46 -0400

    implemented auth

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,29 +1,33 @@
-import { User } from "@/models/User";
-import connectToDB from "@/utils/db";
+import { PrismaClient } from "@prisma/client";
 import bcryptjs from "bcryptjs";
 import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
 
 export const POST = async (req) => {
   const { email, password } = await req.json();
 
-  await connectToDB();
-
-  const existingUser = await User.findOne({ email });
-
-  if (existingUser) {
-    return new NextResponse("User already exists", { status: 400 });
-  }
-
-  const hashedPassword = await bcryptjs.hash(password, 10);
-  const newUser = new User({
-    email: email,
-    password: hashedPassword,
-  });
-
   try {
-    await newUser.save();
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return new NextResponse("User already exists", { status: 400 });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
     return new NextResponse("User created", { status: 200 });
   } catch (error) {
-    return new NextResponse(error, { status: 500 });
+    return new NextResponse(error.message, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
   }
 };
