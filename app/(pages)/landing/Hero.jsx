0e18dd983e7d11b8ba3468cd50db1e4a381c3275
Aryamohan92 <aryamohan613@gmail.com>
2025-03-07 10:48:43 -0400

    update hero section and how it work section

--- app/(pages)/landing/Hero.jsx
+++ app/(pages)/landing/Hero.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Navbar from "@/app/(pages)/_components/Navbar";
-import placeholder from "@/public/placeholder.png";
+import placeholder from "@/public/pet.jpg";
 
 const Hero = () => {
   return (
     <section id="Home" className="relative w-full h-screen">
-      <Navbar />
+     
       {/* Background Image */}
       <div className="w-full">
         <Image
@@ -20,12 +20,12 @@ const Hero = () => {
       {/* Text Overlay */}
       <div className="relative z-10 flex flex-col w-full px-6 pt-[20vh] gap-8 max-md:gap-16 max-md:pt-[15vh] items-center text-center justify-between">
         <h1 className="text-5xl max-md:text-4xl leading-tight font-bold text-white drop-shadow-lg">
-          Headline <br /> goes here
+        Find Your New Best Friend Today!
         </h1>
         <p className="text-xl light-text dark-text drop-shadow-lg font-semibold">
-          Subheading goes here
+        Your new best friend is just a click away
           <br />
-          and more here
+          Browse and adopt today!
         </p>
       </div>
     </section>
--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ import KeyFeatures from "@/app/(pages)/landing/KeyFeatures";
 import Pricing from "@/app/(pages)/landing/Pricing";
 import Hero from "@/app/(pages)/landing/Hero";
 import FAQ from "@/app/(pages)/landing/FAQ";
+import Navbar from "./(pages)/_components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <div className="max-w-[1300px] mx-auto px-6">
         <HowItWorks />
