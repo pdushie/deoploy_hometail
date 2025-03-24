2175315bf254d343ef4af57807b16daf0166111a
Philip Dushie <philip.dushie@gmail.com>
2025-03-24 15:22:47 -0300

    moved navbar and footer to layout

--- app/page.js
+++ app/page.js
@@ -1,22 +1,18 @@
-import Footer from "./(pages)/_components/Footer";
 import HowItWorks from "@/app/(pages)/landing/HowItWorks";
 import KeyFeatures from "@/app/(pages)/landing/KeyFeatures";
 import Pricing from "@/app/(pages)/landing/Pricing";
 import Hero from "@/app/(pages)/landing/Hero";
 import FAQ from "@/app/(pages)/landing/FAQ";
-import Navbar from "./(pages)/_components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <div className="max-w-[1300px] mx-auto px-6">
         <HowItWorks />
         <KeyFeatures />
         <FAQ />
         <Pricing title={"Pricing"} width={"w-[350px]"} />
-        <Footer />
       </div>
     </>
   );
