9d5fc92389d4dd2a7652297239e53b8c528c539a
Philip Dushie <philip.dushie@gmail.com>
2025-03-26 15:47:13 -0300

    Fixes light dark mode text for navbar and footer

--- app/(pages)/_components/Footer.jsx
+++ app/(pages)/_components/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
       <div className="flex flex-col items-center w-full light-text dark-text">
         <footer className="footer footer-center rounded py-4 text-base">
           <nav className="flex gap-6">
-            <a href="APP_NAMEi" className="link link-hover">
+            <a href="APP_NAMEi" className="link link-hover dark:text-white">
               Support
             </a>
-            <Link href="/tos" className="link link-hover">
+            <Link href="/tos" className="link link-hover dark:text-white">
               Terms of Service
             </Link>
-            <Link href="/privacy-policy" className="link link-hover">
+            <Link href="/privacy-policy" className="link link-hover dark:text-white">
               Privacy Policy
             </Link>
           </nav>
